f09ed87be65897c452ce2903c2c96dc09d33c5f7
yujamint <woawoa0513@gmail.com>
2023-06-20 18:53:17 +0900

    chore: 프로젝트 생성

---
+++ akkijang/src/main/java/jejemint/akkijang/AkkijangApplication.java
@@ -0,0 +1,13 @@
+package jejemint.akkijang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkijangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkijangApplication.class, args);
+	}
+
+}
---
+++ akkijang/src/test/java/jejemint/akkijang/AkkijangApplicationTests.java
@@ -0,0 +1,13 @@
+package jejemint.akkijang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkijangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
