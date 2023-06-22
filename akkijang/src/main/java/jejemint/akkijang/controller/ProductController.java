85c3981acc26efc9fa35e224e815b75a6d4aaa58
yujamint <woawoa0513@gmail.com>
2023-06-22 15:04:58 +0900

    refactor(ProductController): API url 수정

--- akkijang/src/main/java/jejemint/akkijang/controller/ProductController.java
+++ akkijang/src/main/java/jejemint/akkijang/controller/ProductController.java
@@ -11,9 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @RestController
 public class ProductController {
 
