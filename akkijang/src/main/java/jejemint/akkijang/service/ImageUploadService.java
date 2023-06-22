215bf99a7b1bfbf34ea04fc937465d5ecb79790f
yujamint <woawoa0513@gmail.com>
2023-06-22 16:30:22 +0900

    refactor: 이미지 저장/전송 경로 프로퍼티 값 설정

--- akkijang/src/main/java/jejemint/akkijang/service/ImageUploadService.java
+++ akkijang/src/main/java/jejemint/akkijang/service/ImageUploadService.java
@@ -2,20 +2,23 @@ package jejemint.akkijang.service;
 
 import java.io.File;
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImageUploadService {
 
-    private static final String uploadDir = "/Users/jaemin/Desktop/workspace/project/akki-jang-be/akkijang/src/main/resources/image/";
+    @Value("${image.dir}")
+    private String imageDir;
+    @Value("${upload.dir}")
+    private String uploadDir;
 
     public String uploadImage(MultipartFile image) {
         try {
             String imageName = image.getOriginalFilename();
-            String imageUrl = uploadDir + imageName;
-            image.transferTo(new File(imageUrl));
-            return imageUrl;
+            image.transferTo(new File(uploadDir + imageName));
+            return imageDir + imageName;
         } catch (IOException e) {
             throw new IllegalArgumentException("파일 업로드에 실패했습니다.", e);
         }
