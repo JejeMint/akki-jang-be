b117a1b3174e224ffc50f938931684e2b0097ae1
yujamint <woawoa0513@gmail.com>
2023-06-24 20:22:09 +0900

    feat(ProductController): 상품 전체 조회 컨트롤러 구현

--- akkijang/src/main/java/jejemint/akkijang/controller/ProductController.java
+++ akkijang/src/main/java/jejemint/akkijang/controller/ProductController.java
@@ -1,10 +1,15 @@
 package jejemint.akkijang.controller;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 import jejemint.akkijang.controller.dto.ProductCreateRequestDto;
+import jejemint.akkijang.controller.dto.ProductSimpleSelectResponseDto;
+import jejemint.akkijang.domain.Product;
 import jejemint.akkijang.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +25,15 @@ public class ProductController {
     @PostMapping("/products")
     public ResponseEntity<Void> createProduct(@ModelAttribute ProductCreateRequestDto request) {
         final Long savedId = productService.createProduct(request);
-
         return ResponseEntity.created(URI.create("/products/" + savedId)).build();
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductSimpleSelectResponseDto>> showProducts() {
+        final List<Product> products = productService.getAllProducts();
+        final List<ProductSimpleSelectResponseDto> responseDto = products.stream()
+                .map(product -> ProductSimpleSelectResponseDto.from(product))
+                .collect(Collectors.toUnmodifiableList());
+        return ResponseEntity.ok(responseDto);
     }
 }
---
+++ akkijang/src/main/java/jejemint/akkijang/controller/dto/ProductSimpleSelectResponseDto.java
@@ -0,0 +1,25 @@
+package jejemint.akkijang.controller.dto;
+
+import java.time.LocalDateTime;
+import jejemint.akkijang.domain.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ProductSimpleSelectResponseDto {
+    private final String title;
+    private final int price;
+    private final String imageUrl;
+    private final String regionCode;
+    private final LocalDateTime createdAt;
+
+    public static ProductSimpleSelectResponseDto from(Product product) {
+        return new ProductSimpleSelectResponseDto(product.getTitle(),
+                product.getPrice(),
+                product.getImageUrl(),
+                product.getRegion().getCode(),
+                product.getCreatedAt());
+    }
+}
